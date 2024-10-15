f2e60ec63901c64995c58710253ed9ac036a4d4f
safernandez9 <safernandez9@gmail.com>
2024-10-15 20:07:50 +0200

    Testing

--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+//Código de prueba durante la instalación
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World! My name is SFS");
--- src/ud1/exercicios/sfs20241010/MultiplosSiete.java
+++ src/ud1/exercicios/sfs20241010/MultiplosSiete.java
@@ -1,3 +1,13 @@
+//Ejercicio de simulacro de examen 1
+
+/*
+ * Escribe un programa que tome como entrada un número entero e indique qué cantidad hay que sumarle para que el 
+ * resultado sea múltiplo de 7. Un ejemplo:
+ * A 2 hay que sumarle 5 para que el resultado (2+5=7) sea múltiplo de 7.
+ * A 13 hay que sumarle 1 para que el resultado (13+1=14) sea múltiplo de 7.
+ * A 14 no hay que sumarle nada (0) para que sea múltiplo de 7
+*/
+
 package ud1.exercicios.sfs20241010;
 
 import java.util.Scanner;
