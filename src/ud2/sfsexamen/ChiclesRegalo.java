b1cec97b5ea0f7060ba51e845a35c0bd6c396f7b
safernandez9 <safernandez9@gmail.com>
2024-11-21 11:26:39 +0100

    Examen Con Testing

--- src/ud2/sfsexamen/ChiclesRegalo.java
+++ src/ud2/sfsexamen/ChiclesRegalo.java
@@ -2,6 +2,10 @@
 
 package ud2.sfsexamen;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
 public class ChiclesRegalo {
 
     public static int totalChicles(int nChiclesComprados, int nEnvoltoriosOferta, int nChiclesGratis) {
@@ -44,5 +48,15 @@ public class ChiclesRegalo {
         } while (nChiclesComprados / nEnvoltoriosOferta != 0);
 
         return chiclesTotales;
+    }
+
+    @Test
+    public void ChiclesRegaloTest() {
+        assertEquals(31, totalChicles(25, 5, 1));
+        assertEquals(6, totalChicles(5, 5, 1));
+        assertEquals(111, totalChicles(100, 10, 1));
+        assertEquals(100, totalChicles(100, 0, 0));
+        assertEquals(-1, totalChicles(20, 2, 5));
+        assertEquals(-1, totalChicles(10, -1, 0));
     }
 }
--- src/ud2/sfsexamen/DuracionBombillas.java
+++ src/ud2/sfsexamen/DuracionBombillas.java
@@ -2,32 +2,44 @@
 
 package ud2.sfsexamen;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.Test;
+
 public class DuracionBombillas {
 
-    public static String causaFinBombilla(int nHoras, int nEncendidos, int horasEstimadas){
+    public static String causaFinBombilla(int nHoras, int nEncendidos, int horasEstimadas) {
 
         int encendidosPosibles;
         String causa = "";
 
-        //Manejo entradas incorrectas
-        if(nHoras <= 0 || nEncendidos <= 0 || horasEstimadas > 10){
-             return "ERROR";
+        // Manejo entradas incorrectas
+        if (nHoras <= 0 || nEncendidos <= 0 || horasEstimadas < 0 || horasEstimadas > 10 || horasEstimadas == 0) {
+            return "ERROR";
         }
-
-        encendidosPosibles = nHoras / horasEstimadas;
         
-        //Manejo los 3 posibles casos
-        if(encendidosPosibles < nEncendidos){
+        encendidosPosibles = nHoras / horasEstimadas;
+
+        // Manejo los 3 posibles casos
+        if (encendidosPosibles < nEncendidos) {
             causa = "HORAS";
-        }
-        else if(encendidosPosibles > nEncendidos){
+        } else if (encendidosPosibles > nEncendidos) {
             causa = "ENCENDIDOS";
-        }
-        else if(encendidosPosibles == nEncendidos){
+        } else if (encendidosPosibles == nEncendidos) {
             causa = "ENCENDIDOS + HORAS";
         }
 
         return causa;
 
-    } 
+    }
+
+    @Test
+    public void causaFinBombillaTest() {
+        assertEquals("ERROR", causaFinBombilla(100, 100, 100));
+        assertEquals("ERROR", causaFinBombilla(1000, -10, 10));
+        assertEquals("ERROR", causaFinBombilla(1000, 10, 0));
+        assertEquals("HORAS", causaFinBombilla(1000, 200, 10));
+        assertEquals("ENCENDIDOS", causaFinBombilla(1000, 100, 1));
+        assertEquals("ENCENDIDOS + HORAS", causaFinBombilla(1000, 100, 10));
+    }
+
 }
