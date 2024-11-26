89f92ab26efe92ff1266e95c98cb53da70871206
safernandez9 <safernandez9@gmail.com>
2024-11-26 12:57:11 +0100

    Prueba Chicles

--- src/ud2/sfsexamen/ChiclesRegalo.java
+++ src/ud2/sfsexamen/ChiclesRegalo.java
@@ -55,8 +55,18 @@ public class ChiclesRegalo {
         assertEquals(31, totalChicles(25, 5, 1));
         assertEquals(6, totalChicles(5, 5, 1));
         assertEquals(111, totalChicles(100, 10, 1));
-        assertEquals(100, totalChicles(100, 0, 0));
         assertEquals(-1, totalChicles(20, 2, 5));
         assertEquals(-1, totalChicles(10, -1, 0));
+        assertEquals(-1, totalChicles(-1, 2, 1));
+        assertEquals(-1, totalChicles(10, 1, -1));
+        assertEquals(-1, totalChicles(20, 2, 2));
+        assertEquals(10, totalChicles(7, 3, 1));
+        assertEquals(40, totalChicles(27, 3, 1));
+        assertEquals(21, totalChicles(11, 2, 1));
+        assertEquals(31, totalChicles(16, 2, 1));
+        assertEquals(100, totalChicles(100, 0, 0));
+        assertEquals(100, totalChicles(100, 0, 1));
+        assertEquals(0, totalChicles(0, 0, 1));
+        assertEquals(0, totalChicles(0, 2, 1));
     }
 }
