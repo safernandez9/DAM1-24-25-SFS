14b37b04dd7667fe761fe6c2894a7a836e877864
safernandez9 <safernandez9@gmail.com>
2024-11-27 13:18:38 +0100

    Apuntes POO

--- src/ud3/ejemplos/FechaMain.java
+++ src/ud3/ejemplos/FechaMain.java
@@ -20,7 +20,5 @@ public class FechaMain {
         } else {
             System.out.println("Fecha no valida");
         }
-        sc.close();
     }
 }    
-
--- src/ud3/ejemplos/Persona.java
+++ src/ud3/ejemplos/Persona.java
@@ -11,12 +11,36 @@ public class Persona {
 
     public static int contadorPersonas;
     
-    //Métodos
+
+    // CONSTRUCTORES
+    Persona() {
+    }    
+
+    Persona(String nombre) {
+        this.nombre = nombre;
+    }    
+    Persona (String nombre, int edad, double estatura) {
+        this.nombre = nombre; //asigna el parámetro al atributo
+        this.edad = edad;
+        this.estatura = estatura;
+     }
+  
+    Persona(String nombre, int edad) {                                                                     
+        this.nombre = nombre;
+        this.edad = edad;
+        contadorPersonas++;
+    }    
+
+
+
+    // Otros Métodos
+
     public void inicializar(String n, String a1, String a2, int e){                                            
            nombre = n;
            apellido1 = a1;
            apellido2 = a2;
            edad = e;
+           contadorPersonas++;
     }
     
     public String nombreCompleto() {
@@ -39,7 +63,32 @@ public class Persona {
         return edad;
     }
 
+
+
+    public double getEstatura() {
+        return estatura;
+    }
+
+    public void setEstatura(double estatura) {
+
+        if (estatura > 0)
+            this.estatura = estatura;
+    }
+
+    void crecer(double incremento) {
+        estatura += incremento; 
+    }
+    
+    
+
     public static void incrementarContador(){
         contadorPersonas++;
     }    
+
+    void saludar() {
+        System.out.println("Hola. Mi nombre es " + nombre);
+        System.out.println("Encantado de conocerte.");
+    }
+
+
 }
