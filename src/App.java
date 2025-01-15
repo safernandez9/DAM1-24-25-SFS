ac5d4cb05c4907acfbbe47ffcf56779d1a5ef95a
safernandez9 <safernandez9@gmail.com>
2025-01-15 12:21:46 +0100

    No se que hice

--- src/App.java
+++ src/App.java
@@ -2,6 +2,6 @@
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World! My name is SFS");
+        System.out.println("Hello, World! My name is SFS2");
     }
 }
