605e7375d85abe23d09a4e4ac2d9b91768e3d4c7
safernandez9 <safernandez9@gmail.com>
2025-04-24 12:47:55 +0200

    UPD

--- src/ud6/festivalmeigas/Feitizo.java
+++ src/ud6/festivalmeigas/Feitizo.java
@@ -5,104 +5,108 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
- import java.util.Map;
+import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
 
 public class Feitizo {
-       private String nome;
-       private List<String> ingredientes;
-       private int nivelDificultade;
+    private String nome;
+    private List<String> ingredientes;
+    private int nivelDificultade;
 
-       public Feitizo(String nome, String[] ingredientes, int nivelDificultade) {
+    public Feitizo(String nome, String[] ingredientes, int nivelDificultade) {
         this.nome = nome;
         this.ingredientes = Arrays.asList(ingredientes);
         this.nivelDificultade = nivelDificultade;
-       }
+    }
 
-       public String getNome() {
-           return nome;
-       }
+    public String getNome() {
+        return nome;
+    }
 
-       public void setNome(String nome) {
-           this.nome = nome;
-       }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-       public List<String> getIngredientes() {
-           return ingredientes;
-       }
+    public List<String> getIngredientes() {
+        return ingredientes;
+    }
 
-       public void setIngredientes(List<String> ingredientes) {
-           this.ingredientes = ingredientes;
-       }
+    public void setIngredientes(List<String> ingredientes) {
+        this.ingredientes = ingredientes;
+    }
 
-       public int getNivelDificultade() {
-           return nivelDificultade;
-       }
+    public int getNivelDificultade() {
+        return nivelDificultade;
+    }
 
-       public void setNivelDificultade(int nivelDificultade) {
-           this.nivelDificultade = nivelDificultade;
-       }
+    public void setNivelDificultade(int nivelDificultade) {
+        this.nivelDificultade = nivelDificultade;
+    }
 
-       public static void main(String[] args) {
-        
+    public static void main(String[] args) {
+
         Feitizo[] feitizos = crearFeitizosExemplo();
         System.out.println(ingredientesUnicos(List.of(feitizos)));
-       }
+    }
 
+    public static Feitizo[] crearFeitizosExemplo() {
+        String[] ingredientes1 = { "auga de río encantada", "pétalos de rosa silvestre", "fume de lareira" };
+        String[] ingredientes2 = { "auga do mar", "salicornia", "berberechos místicos" };
+        String[] ingredientes3 = { "raíz de toxo", "auga bendita", "po de estrela fugaz" };
+        String[] ingredientes4 = { "auga de río encantada", "raíz de toxo", "pel de serpe" };
+        String[] ingredientes5 = { "auga do mar", "po de estrela fugaz", "cinza de bidueiro" };
+        String[] ingredientes6 = { "auga bendita", "pétalos de rosa silvestre", "brétema das fragas" };
 
-       public static Feitizo[] crearFeitizosExemplo() {
-        String[] ingredientes1 = {"auga de río encantada", "pétalos de rosa silvestre", "fume de lareira"};
-        String[] ingredientes2 = {"auga do mar", "salicornia", "berberechos místicos"};
-        String[] ingredientes3 = {"raíz de toxo", "auga bendita", "po de estrela fugaz"};
-        String[] ingredientes4 = {"auga de río encantada", "raíz de toxo", "pel de serpe"};
-        String[] ingredientes5 = {"auga do mar", "po de estrela fugaz", "cinza de bidueiro"};
-        String[] ingredientes6 = {"auga bendita", "pétalos de rosa silvestre", "brétema das fragas"};
-    
         Feitizo f1 = new Feitizo("Encantamento da Chuvia Mansa", ingredientes1, 3);
         Feitizo f2 = new Feitizo("Invocación Mariña", ingredientes2, 4);
         Feitizo f3 = new Feitizo("Luz do Luar", ingredientes3, 2);
         Feitizo f4 = new Feitizo("Garda da Fraga", ingredientes4, 3);
         Feitizo f5 = new Feitizo("Respiro Oceánico", ingredientes5, 4);
         Feitizo f6 = new Feitizo("Bendición Floral", ingredientes6, 2);
-    
-        return new Feitizo[]{f1, f2, f3, f4, f5, f6};
+
+        return new Feitizo[] { f1, f2, f3, f4, f5, f6 };
     }
 
-    public static Collection<String> ingredientesUnicos(Collection<Feitizo> feitizos){
+    public static Collection<String> ingredientesUnicos(Collection<Feitizo> feitizos) {
         Set<String> ingredientesUnicos = new TreeSet<>();
-        for(Feitizo f: feitizos){
+        for (Feitizo f : feitizos) {
             ingredientesUnicos.addAll((f.getIngredientes()));
         }
-        
-            return ingredientesUnicos;
-        
+
+        return ingredientesUnicos;
+
     }
 
-    public static List<Feitizo> feitizosPosibles(Set<String> ingredientes) {
-        List<Feitizo> Posibles = new ArrayList<>();
-        for(Feitizo f: feitizos){
-
-        }
-    }
-       
-    public static Map<String, Integer> ingredientesVecesUsados(Collection<Feitizo> feitizos) {
-        Map<String, Integer> contador = new HashMap();
-        for(Feitizo f: feitizos){
-            for(String ingrediente: f.getIngredientes()){
-                contador.put(ingrediente,contador.getOrDefault(ingrediente, 0) + 1);
+    public static List<Feitizo> feitizosPosibles(Set<String> ingredientes, Collection<Feitizo> feitizos) {
+        List<Feitizo> posibles = new ArrayList<>();
+        for (Feitizo f : feitizos) {
+            if (Arrays.stream(f.getIngredientes()).allMatch(ingredientes::contains)) {
+                posibles.add(f);
             }
         }
-                return contador;
+        return posibles;
     }
-    
+
+    public static Map<String, Integer> ingredientesVecesUsados(Collection<Feitizo> feitizos) {
+        Map<String, Integer> contador = new HashMap();
+        for (Feitizo f : feitizos) {
+            for (String ingrediente : f.getIngredientes()) {
+                contador.put(ingrediente, contador.getOrDefault(ingrediente, 0) + 1);
+            }
+        }
+        return contador;
+    }
+
     public boolean addIngrediente(String ingrediente) {
         return true;
     }
+
     public boolean removeIngrediente(String ingrediente) {
         return true;
     }
-    public boolean cambiarIngrediente(String ingredienteViejo, String ingredienteNuevo){
+
+    public boolean cambiarIngrediente(String ingredienteViejo, String ingredienteNuevo) {
         return true;
     }
 
